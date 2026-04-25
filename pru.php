375ae79bc77ba3022ce53f09ff771c75ca7590ce
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-04-24 18:32:54 -0600

    Create pru.php

---
+++ pru.php
@@ -0,0 +1,13 @@
+<?php
+// for testing
+// MIT
+// Alfonso Orozco Aguilar
+// para mostrar errores en php7
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//require_once "index.php";
+require_once "fleet_login.php";
+
+?>
