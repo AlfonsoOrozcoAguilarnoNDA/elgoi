191151b1dc4d5147b86dd8aacf32c19ee1f593ef
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-04-26 09:18:52 -0600

    Create pru.php

---
+++ crew/pru.php
@@ -0,0 +1,13 @@
+<?php
+// for testing
+// License GPL 3.0
+// Alfonso Orozco Aguilar
+// para mostrar errores en php7
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//require_once "index.php";
+require_once "moneymakers.php";
+
+?>
