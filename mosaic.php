19426cfe0a82becd19b19c90cf2ece81eddfdcf5
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-05-28 17:39:19 -0600

    Update mosaic.php

--- mosaic.php
+++ mosaic.php
@@ -67,7 +67,8 @@ $tiles = [
     
 ];
 
-$tiles2 = [    
+$tiles2 = [
+	showTile("industry/slots.php", "fa-industry", "Factory Slots"),
     showTile("abyss/crew.php", "fa-users", "primary"),
     showTile("logistics/hauling.php", "fa-truck", "secondary"),    
     showTile("intel/spy.php", "fa-eye", "dark")  
