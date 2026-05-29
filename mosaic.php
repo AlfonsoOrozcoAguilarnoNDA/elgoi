696046eb5ace97003d4eb11b0735fb9987437349
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-05-28 18:10:40 -0600

    Update mosaic.php

--- mosaic.php
+++ mosaic.php
@@ -69,7 +69,7 @@ $tiles = [
 $tiles2 = [
 	showTile("crew/skill_inspector.php", "fa-clipboard-check", "primary"),
 	showTile("industry/slots.php", "fa-industry", "info"),
-	showTile("industry/calc_p1.php", "fa-industry", "info"),
+	showTile("industry/calc_p1.php", "fa-calculator", "info"),
 	showTile("experimental/tradehubs_ammo.php", "fa-chart-line", "success"),
 	showTile("experimental/whereare.php", "fa-globe", "success"),
     showTile("experimental/thesix.php", "fa-users", "primary"),
