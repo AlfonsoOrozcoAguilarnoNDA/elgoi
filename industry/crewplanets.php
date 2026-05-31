e3a1375a05676be65b9f4c7af001e099b17dd71b
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-05-31 10:53:32 -0600

    Update crewplanets.php

--- industry/crewplanets.php
+++ industry/crewplanets.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * EVE Online - Unified Audit Tool
- * License: MIT
+ * EVE Online - Unified Crew Audit Tool
+ * License: GPL 2.0
  * Author: Alfonso Orozco Aguilar
  * Tabs: Graduation | Reputation | Biometrics | Evermarks
  * Stack: PHP 8.x Procedural, MariaDB, Bootstrap 4.6.2, Font Awesome 5.15.4
@@ -307,7 +307,7 @@ function renderTabReputation() {
                         <span class="trade-pill"><?php echo htmlspecialchars($tradefield); ?></span>
                     <?php else: ?><small class="text-muted">-</small><?php endif; ?>
                 </td>
-                <td><?php echo htmlspecialchars($row['target_description']); ?></td>
+                <td style='color:#ffffff;'><?php echo htmlspecialchars($row['target_description']); ?></td>
                 <td class="text-right reputation-num <?php echo $rep_class; ?>"><?php echo number_format($rep, 2); ?></td>
                 <td class="text-center">
                     <span class="pocket-badge-dip" style="background-color:<?php echo get_pocket_color($p6_val); ?>;color:<?php echo get_pocket_text($p6_val); ?>;">
@@ -736,7 +736,7 @@ echo crew_navbar();
             background-color: #0b0c0e;
             color: #ced4da;
             font-family: 'Segoe UI', sans-serif;
-            padding-top: 120px; /* navbar (~60px) + eve-tabs (~56px) */
+            padding-top: 40px; /* navbar (~60px) + eve-tabs (~56px) */
             padding-bottom: 70px;
         }
 
