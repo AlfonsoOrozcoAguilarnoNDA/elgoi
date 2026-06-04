1adb3e2a229fe8383883a3642cebd0674048ad9c
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-04 08:15:05 -0600

    Update pru.php

--- crew/pru.php
+++ crew/pru.php
@@ -8,6 +8,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //require_once "index.php";
-require_once "compare.php";
+require_once "moneymakers.php";
 
 ?>
