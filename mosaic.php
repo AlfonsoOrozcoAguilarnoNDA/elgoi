28caed400c35d8e8cd710e37ac855c276543c6c4
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-21 10:04:03 -0600

    Update mosaic.php
    more icon reorder

--- mosaic.php
+++ mosaic.php
@@ -50,6 +50,7 @@ $tiles = [
     showTile("crew/career_plan.php", "fa-sitemap", "dark"),
     showTile("crew/compare.php", "fa-balance-scale", "light"),
     showTile("specific/balance.php", "fa-coins", "warning"),
+	showTile("industry/slots.php", "fa-industry", "info"),
     showTile("specific/get_market.php", "fa-chart-line", "success"),
     showTile("specific/skybreaker.php", "fa-rocket", "danger"),
 	showTile("specific/explode.php", "fa-bomb", "light"),    
@@ -65,8 +66,7 @@ $tiles = [
 
 $tiles2 = [	
 	showTile("experimental/kimitest4.php", "fa-chart-pie", "primary"),
-	showTile("experimental/unsafe_station.php", "fa-truck-loading", "danger"),
-	showTile("industry/slots.php", "fa-industry", "info"),
+	showTile("experimental/unsafe_station.php", "fa-truck-loading", "danger"),	
 	showTile("industry/ammo.php", "fa-industry", "info"),
 	showTile("industry/calc_p1.php", "fa-calculator", "info"),	
 	showTile("experimental/whereare.php", "fa-globe", "success"),
