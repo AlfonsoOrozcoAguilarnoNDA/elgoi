6b33976c0911988d88220613f1035ce4ab7db3f2
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-22 10:42:02 -0600

    Update apex_roster.php
    debugging

--- crew/apex_roster.php
+++ crew/apex_roster.php
@@ -15,7 +15,7 @@ echo ui_generate_navbar();
 echo "<div style='overflow-x: auto;' class='row flex-row flex-nowrap ml-3 mt-4 pb-4 pt-2'><div>";
 
 $pocket="";
-$usuario=$_SESSION['pilot_name']; ; // debugm is the supergroup
+$usuario=$_SESSION['character_id']; // debugm is the supergroup
 set_time_limit(60);
 echo Showdashboard_skills($pocket,$usuario);
 
