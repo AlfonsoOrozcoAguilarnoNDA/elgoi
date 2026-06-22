2f623a6d975d4137b0a6d9e8cf2ecc3b2eab8cea
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-22 15:59:46 -0600

    Update apex_roster.php

--- crew/apex_roster.php
+++ crew/apex_roster.php
@@ -240,7 +240,8 @@ if ($result2 = mysqli_query($link, $sql)) {
     if ($quien==$obj->toon_name and $obj2->rank > 0){
       $ren++;
       $visible += $obj2->skillpoints;
-      $enlace="<a target='_blank' href='abyss/skill_detail.php?module=dt2&what=$obj2->typeID'>$obj2->typeID</a>";
+      $enlace="<a target='_blank' href='abyss/skill_detail.php?module=dt2&what=$obj2->typeID'>$obj2->typeID</a>"; // old style
+      $enlace="<a target='_blank' href='skillinspector.php?skill_id=$obj2->typeID&pocket6=ALL'>$obj2->typeID</a>";      
       list($maxa)=avalues319b("select EXPANDED from ALPHA_CLONES where numberskill=$obj2->typeID");
       $sty="";
       if ($maxa>0) $sty=" style='background-color:#ffc0cb'";
