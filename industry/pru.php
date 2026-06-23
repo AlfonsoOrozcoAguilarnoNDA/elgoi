2ea4069c846a1fc8e6b01a3aaf46f2827cfaf953
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-22 19:11:29 -0600

    Update pru.php
    change depuration file

--- industry/pru.php
+++ industry/pru.php
@@ -8,6 +8,5 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //require_once "index.php";
-require_once "calc_p1.php";
-
+require_once "nventory_pi.php";
 ?>
