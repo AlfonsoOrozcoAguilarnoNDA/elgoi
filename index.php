533dee4967bf4e568d27c8a65db131b883b52a8c
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-09-14 10:08:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0',false);
 header('Pragma: no-cache');
 
+// Evitar indexación por motores de búsqueda
+header('X-Robots-Tag: noindex, nofollow, noarchive', true);
+
 //date_default_timezone_set('UTC'); // Potential for mistakes
 date_default_timezone_set('America/Mexico_City');
 
